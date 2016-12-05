3bda5fce78b4abaf5f854878ae27abcb30a59228
keel <keel.sike@gmail.com>
2016-12-05 13:46:48 +0800

    增加及趣API的JAVA接口放在tools里面

--- src/com/k99k/tools/ApiUtil.java
+++ src/com/k99k/tools/ApiUtil.java
@@ -4,9 +4,9 @@
 package com.k99k.tools;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.MessageDigest;
@@ -30,6 +30,8 @@ public class ApiUtil {
 	 */
 	public ApiUtil() {
 	}
+	
+	
 
 	/**
 	 * post数据到一个url,并获取反回的String,需要拼合参数
@@ -49,6 +51,8 @@ public class ApiUtil {
 	public final static String postUrl(String url, String data,
 			String[] headerKeys, String[] headerValues, int timeOut,
 			boolean breakLine, String charset) {
+		String out = null;
+
 		try {
 			URL aUrl = new URL(url);
 			URLConnection conn = aUrl.openConnection();
@@ -81,67 +85,87 @@ public class ApiUtil {
 					sb.append(line);
 				}
 			}
+			out = sb.toString();
 			wr.close();
 			rd.close();
 			return sb.toString();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return out;
+		}
+
+	}
+
+	public static String md5(String str) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(str.getBytes());
+			byte encrypt[];
+			encrypt = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte t : encrypt) {
+                String s = Integer.toHexString(t & 0xFF);
+                if (s.length() == 1) {
+                	sb.append(0);
+                }
+                sb.append(s);
+            }
+            String re = sb.toString();
+            return re;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	
-	public static String md5(String str) {
-	    try {
-	        MessageDigest md = MessageDigest.getInstance("MD5");
-	        md.update(str.getBytes());
-	        return new BigInteger(1, md.digest()).toString(16);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
-	
 	@SuppressWarnings("unchecked")
-	private static final TreeMap<String,Object> pushSortMap(TreeMap<String,Object> sortMap,HashMap<String,Object> data){
-		for (Map.Entry<String,Object> entry : data.entrySet()) { 
-			if(entry.getKey().equals("req")){
-				sortMap = pushSortMap(sortMap,(HashMap<String, Object>) entry.getValue());
+	private static final TreeMap<String, Object> pushSortMap(
+			TreeMap<String, Object> sortMap, HashMap<String, Object> data) {
+		for (Map.Entry<String, Object> entry : data.entrySet()) {
+			if (entry.getKey().equals("req")) {
+				sortMap = pushSortMap(sortMap,
+						(HashMap<String, Object>) entry.getValue());
 				continue;
 			}
-			//签名只考虑数字和string类型
-		    if (entry.getValue() instanceof String || entry.getValue() instanceof Integer || entry.getValue() instanceof Long || entry.getValue() instanceof Float) {
+			// 签名只考虑数字和string类型
+			if (entry.getValue() instanceof String
+					|| entry.getValue() instanceof Integer
+					|| entry.getValue() instanceof Long
+					|| entry.getValue() instanceof Float) {
 				sortMap.put(entry.getKey(), entry.getValue());
 			}
 		}
 		return sortMap;
 	}
-	
-	public static String mkSign(HashMap<String,Object> data,String key){
-		TreeMap<String,Object> sortMap = new TreeMap<String, Object>();
-		sortMap = pushSortMap(sortMap,data);
+
+	public static String mkSign(HashMap<String, Object> data, String key) {
+		TreeMap<String, Object> sortMap = new TreeMap<String, Object>();
+		sortMap = pushSortMap(sortMap, data);
 		Set<String> keySet = sortMap.keySet();
-        Iterator<String> iter = keySet.iterator();
-        StringBuilder sb = new StringBuilder();
-        while (iter.hasNext()) {
-            String sortKey = iter.next();
-            sb.append(sortKey);
-            sb.append("=");
-            sb.append(sortMap.get(sortKey));
-            sb.append("&");
-        }
-        sb.append("key=").append(key);
-        String signStr = sb.toString();
-//        System.out.println("signStr:"+signStr);
+		Iterator<String> iter = keySet.iterator();
+		StringBuilder sb = new StringBuilder();
+		while (iter.hasNext()) {
+			String sortKey = iter.next();
+			sb.append(sortKey);
+			sb.append("=");
+			sb.append(sortMap.get(sortKey));
+			sb.append("&");
+		}
+		sb.append("key=").append(key);
+		String signStr = sb.toString();
+		 System.out.println("signStr:"+signStr);
 		return md5(signStr);
 	}
-	
-	public static final long timeStamp(){
+
+	public static final long timeStamp() {
 		return new Date().getTime();
 	}
-	
-	public static final String mkApiReq(String method, HashMap<String,Object> reqData,String key,String appCode,String channel,int ver){
-		HashMap<String,Object> reqMap = new HashMap<String, Object>();
+
+	public static final String mkApiReq(String method,
+			HashMap<String, Object> reqData, String key, String appCode,
+			String channel, int ver) {
+		HashMap<String, Object> reqMap = new HashMap<String, Object>();
 		reqMap.put("m", method);
 		reqMap.put("v", ver);
 		reqMap.put("a", appCode);
@@ -151,31 +175,41 @@ public class ApiUtil {
 		String sign = mkSign(reqMap, key);
 		reqMap.put("s", sign);
 		String reqStr = JSON.write(reqMap);
-		System.out.println("mkApiReq:"+reqStr);
+//		System.out.println("mkApiReq:" + reqStr);
 		return reqStr;
 	}
-	
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String url = "http://localhost:15000/fake/increase";
+		
+		String url = "http://58.223.2.136/forwardapi/api/forward";
+//		String url = "http://localhost:13010/api/forward";
 		HashMap<String, Object> req = new HashMap<String, Object>();
-		req.put("userName", "aa");
-		String data = mkApiReq("increase", req, "testkey", "jquer", "12001", 1);
-		System.out.println("data:"+data);
+		req.put("ver", 1);
+		req.put("re", 10);
+		req.put("phone", "15301588025");
+		req.put("act", 0);
+		req.put("msg", "msg");
+		req.put("seq", "seq111");
+		req.put("channelId", "1231111");
+		req.put("serviceKey", "41oLF2pyfEfX");
+		String data = mkApiReq("forward", req, "NvKSqtMS39OD", "commic", "12001", 1);
+		System.out.println("data:" + data);
 		int timeOut = 2000;
-//		String[] headerKeys = {"Cookie" };
-//		String[] headerValues = {"kie_kc_1=gLoCputwu%2F6W5jVB9%2BSzTn8IaYtwk2sbEJ0fUPgUaIFmOM%2FiWri41MdFK3eD4ZUxlo4A7gsqUcPv0YvChL4mZQ%3D%3D" };
-//		String re = postUrl(url, data, headerKeys, headerValues, timeOut, true,"utf-8");
-		
-		
-		String re = postUrl(url, data, null, null, timeOut, true,"utf-8");
+		// String[] headerKeys = {"Cookie" };
+		// String[] headerValues =
+		// {"kie_kc_1=gLoCputwu%2F6W5jVB9%2BSzTn8IaYtwk2sbEJ0fUPgUaIFmOM%2FiWri41MdFK3eD4ZUxlo4A7gsqUcPv0YvChL4mZQ%3D%3D"
+		// };
+		// String re = postUrl(url, data, headerKeys, headerValues, timeOut,
+		// true,"utf-8");
+
+		String re = postUrl(url, data, null, null, timeOut, true, "utf-8");
 		System.out.println(re);
-		HashMap<String,Object> reObj = (HashMap<String, Object>) JSON.read(re);
-		System.out.println(reObj.get("re"));
-		
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		System.out.println(reObj);
+
 	}
 
 }
--- src/com/k99k/tools/JfApi.java
+++ src/com/k99k/tools/JfApi.java
@@ -7,10 +7,12 @@ public class JfApi {
 	private int ver = 1;
 	private String appCode = "jquer";
 	private String channel = "12001";
-	private String key = "testkey";
+	private String key = "STaDs5GnkBXAqxRq";
 
-//	private String apiUrlPre = "http://localhost:15000/fake/";
-	private String apiUrlPre = "http://kf.loyoo.co/jquer/fake/";
+	private String apiUrlPre = "http://localhost:15001/jifen/";
+	private String apiUrlPre2 = "http://localhost:15001/card/";
+//	private String apiUrlPre = "http://kf.loyoo.co/jquer/jifen/";
+//	private String apiUrlPre2 = "http://kf.loyoo.co/jquer/card/";
 	int timeOut = 2000;
 	private String encode = "utf-8";
 
@@ -37,11 +39,194 @@ public class JfApi {
 		return reObj;
 	}
 	
+	public HashMap<String, Object> decrease(String pointSeq,String pointReqTranSeq,int pointAmount, int subType,String attach,String customerId,String orderSeq) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("pointSeq", pointSeq);
+		req.put("pointReqTranSeq", pointReqTranSeq);
+//		req.put("reqDate", reqDate);
+		req.put("pointAmount", pointAmount);
+		req.put("subType", subType);
+		req.put("attach", attach);
+		req.put("customerId", customerId);
+		req.put("orderSeq", orderSeq);
+		String data = ApiUtil.mkApiReq("decrease", req, key, appCode, channel,
+				ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre + "decrease", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	/**
+	 * 查询余额
+	 * @param customerId
+	 * @return
+	 */
+	public HashMap<String, Object> userBalance(String customerId) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("customerId", customerId);
+		String data = ApiUtil.mkApiReq("userBalance", req, key, appCode, channel,
+				ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre + "userBalance", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	/**
+	 * 查询流水
+	 * @param customerId
+	 * @param startTime "yyyy-mm-dd hh:mm:ss"
+	 * @param endTime "yyyy-mm-dd hh:mm:ss"
+	 * @return
+	 */
+	public HashMap<String, Object> pointLog(String customerId,String startTime,String endTime,int type,String val) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("customerId", customerId);
+		req.put("startTime", startTime);
+		req.put("endTime", endTime);
+		req.put("type", type);
+		req.put("val", val);
+		String data = ApiUtil.mkApiReq("pointLog", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre + "pointLog", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	
+	//创建一批卡
+	public HashMap<String, Object> createCards(int fee,String reason,String createUser,int createUserId,int batchCount,String expireTime,int type) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("fee", fee);
+		req.put("reason", reason);
+		req.put("createUser", createUser);
+		req.put("createUserId", createUserId);
+		req.put("batchCount", batchCount);
+		req.put("expireTime", expireTime);
+		req.put("type", type);
+		String data = ApiUtil.mkApiReq("createCards", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre2 + "createCards", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	
+	//使用卡充积分
+	public HashMap<String, Object> useCard(String cardNo,String cardPwd,String customerId,String orderSeq) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("cardNo", cardNo);
+		req.put("cardPwd", cardPwd);
+		req.put("customerId", customerId);
+		req.put("orderSeq", orderSeq);
+		String data = ApiUtil.mkApiReq("useCard", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre2 + "useCard", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	
+	//更新单张卡状态,0为有效,1为已使用,2为暂停,3为冻结,4为禁用
+	public HashMap<String, Object> updateCard(String cardNo,int state) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("cardNo", cardNo);
+		req.put("state", state);
+		String data = ApiUtil.mkApiReq("updateCard", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre2 + "updateCard", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	//更新卡批次,主要是卡延期
+	public HashMap<String, Object> updateCardBatch(int batchNo,String expireTime) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("batchNo", batchNo);
+		req.put("expireTime", expireTime);
+		String data = ApiUtil.mkApiReq("updateCardBatch", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre2 + "updateCardBatch", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	
+	//获取单卡信息
+	public HashMap<String, Object> cardInfo(String cardNo) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("cardNo", cardNo);
+		String data = ApiUtil.mkApiReq("cardInfo", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre2 + "cardInfo", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
+	
+	//获取卡批次信息
+	public HashMap<String, Object> batchInfo(int batchNo) {
+		HashMap<String, Object> req = new HashMap<String, Object>();
+		req.put("batchNo", batchNo);
+		String data = ApiUtil.mkApiReq("batchInfo", req, key, appCode, channel,ver);
+		System.out.println("data:" + data);
+		String re = ApiUtil.postUrl(apiUrlPre2 + "batchInfo", data, null, null, timeOut,
+				true, encode);
+		System.out.println(re);
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> reObj = (HashMap<String, Object>) JSON.read(re);
+		
+		return reObj;
+	}
+	
 	public static void main(String[] args) {
 		JfApi jf = new JfApi();
-		HashMap<String, Object> re = jf.increase("pointSeq1","pointReqTranSeq1",100,1,"attach","customerId",1,10);
-		System.out.println(re.get("re"));
-		System.out.println(re.get("data"));
+		HashMap<String, Object> re = null;
+//		re = jf.pointLog("customer1", "2016-09-22 15:19:14", "2016-09-23 18:19:14",1,"customer1");
+//		re = jf.userBalance("customer1");
+//		re = jf.increase("pointSeq1233","pointReqTranSeq1",100,1,"中文字","customer1",1,10);
+//		System.out.println(re);
+//		re = jf.decrease("pointSe123","pointReqTranSeq12",120,1,"attach","customer1","orderSeq111");
+//		re = jf.createCards(500, "搞活动送500积分", "keel", 1, 20, "2016-12-30 23:13:19", 1);
+//		re = jf.useCard("500000798918", "963837", "customer1", "orderSeq112");
+//		re = jf.updateCard("500000798918", 4);
+//		re = jf.updateCardBatch(5, "2016-12-30 23:53:19");
+//		re = jf.cardInfo("500000798918");
+		re = jf.batchInfo(5);
+		System.out.println(re);
 		
 	}
 	
